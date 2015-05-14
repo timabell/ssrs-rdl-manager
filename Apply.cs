fb17f118578c05724a7ee4901dbd303da7284ca4
Tim Abell <tim@timwise.co.uk>
2015-05-14 19:17:10 +0100

    use the arg parser fork
    original is abandonware

--- Apply.cs
+++ Apply.cs
@@ -1,11 +1,13 @@
-﻿namespace SsrsRdlManager.Apply
+﻿using CommandLine.Text;
+
+namespace SsrsRdlManager.Apply
 {
     class ConsoleApp
     {
         static void Main(string[] args)
         {
-            var opts = new Options();
-            var parsedArgs = CommandLine.Parser.Default.ParseArguments(args, opts);
+            var parsedArgs = CommandLine.Parser.Default.ParseArguments<Options>(args);
+            var opts = parsedArgs.Value;
             var ta = new TemplateApplier(opts.template, opts.target);
             ta.Apply();
         }

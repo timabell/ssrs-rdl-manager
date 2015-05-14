e8ee6575d59e684776b14d8ec7b110a3c87279c6
Tim Abell <tim@timwise.co.uk>
2015-05-14 13:27:30 +0100

    blank console project
    generated by VS 2013

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ssrs_rdl_manager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

39cf5ea2d1bc916e2b8505d87af87591d6bb4c72
Tim Abell <tim@timwise.co.uk>
2015-05-14 17:19:56 +0100

    some kind of structure

--- Apply.cs
+++ Apply.cs
@@ -1,10 +1,19 @@
 ﻿namespace SsrsRdlManager.Apply
 {
-    class Apply
+    class ConsoleApp
     {
         static void Main(string[] args)
         {
+            var opts = new Options();
+            var parsedArgs = CommandLine.Parser.Default.ParseArguments(args, opts);
+            var ta = new TemplateApplier(opts.template, opts.target);
+            ta.Apply();
+        }
 
+        class Options
+        {
+            public string template { get; set; }
+            public string target { get; set; }
         }
     }
 }
---
+++ TemplateApplier.cs
@@ -0,0 +1,19 @@
+﻿namespace SsrsRdlManager.Apply
+{
+    public class TemplateApplier
+    {
+        private string _templateFile;
+        private string _targetFile;
+
+        public TemplateApplier(string templateFile, string targetFile)
+        {
+            _templateFile = templateFile;
+            _targetFile = targetFile;
+        }
+
+        public void Apply()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

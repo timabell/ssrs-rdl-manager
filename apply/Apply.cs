f9273c6573e5146f8bec3fb7351110f94aa96c49
Tim Abell <tim@timwise.co.uk>
2015-05-15 14:22:05 +0100

    wip on options parsing

--- apply/Apply.cs
+++ apply/Apply.cs
@@ -1,21 +1,31 @@
-﻿using CommandLine.Text;
+﻿using System.Linq;
 
 namespace SsrsRdlManager.Apply
 {
+    using CommandLine;
+
     class ConsoleApp
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            var parsedArgs = CommandLine.Parser.Default.ParseArguments<Options>(args);
+            var parsedArgs = Parser.Default.ParseArguments<Options>(args);
+            if (parsedArgs.Errors.Any())
+            {
+                return 1;
+            }
             var opts = parsedArgs.Value;
-            var ta = new TemplateApplier(opts.template, opts.target);
+            var ta = new TemplateApplier(opts.Template, opts.Target);
             ta.Apply();
+            return 0;
         }
 
         class Options
         {
-            public string template { get; set; }
-            public string target { get; set; }
+            [Option]
+            public string Template { get; set; }
+
+            [Option]
+            public string Target { get; set; }
         }
     }
 }
--- apply/TemplateApplier.cs
+++ apply/TemplateApplier.cs
@@ -13,7 +13,6 @@
 
         public void Apply()
         {
-            throw new System.NotImplementedException();
         }
     }
 }
